6a1e3df9e76b4524b703b05b5149c8e6b4e0501f
Mr.President <41217710+mariklolik@users.noreply.github.com>
2022-06-26 01:55:02 +0700

    Update main.py

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ from dbremote import db_session
 from flask import Flask
 import os
 import json
-import api
+import api_new
 
 app = Flask(__name__)
 app.config["SECRET_KEY"] = "sk"
@@ -24,7 +24,7 @@ def not_found_error():
 def main():
     db_session.global_init("db/data.sqlite")
     port = int(os.environ.get("PORT", 80))
-    app.register_blueprint(api.blueprint)
+    app.register_blueprint(api_new.blueprint)
     app.run(port=port, host='0.0.0.0', debug=False)
     return 0
 
